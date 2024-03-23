3a54a4b0b56c9b709926284683a0d56bf4425920
EmaFerrao <125159850+EmaFerrao@users.noreply.github.com>
2024-03-23 07:50:43 +0000

    prints

--- parque.c
+++ parque.c
@@ -48,6 +48,9 @@ char* le_nome_parque(char* linha, int* posicao) {
     } else { 
         while(!caracterbranco(linha[*posicao]) && linha[*posicao] != '\0') {
             copia_char_incrementa_posicao(buffer, linha, posicao, &i);
+            printf("buffer: %s\n", buffer);
+            printf("posicao: %d", posicao);
+            printf("linha[*posicao]: %s\n", linha[*posicao]);
         }
     }
     buffer[i] = '\0';
