ffc4f6b80aceaa944b7652abccc2bcf33b585ab8
EmaFerrao <125159850+EmaFerrao@users.noreply.github.com>
2024-03-27 19:38:21 +0000

    custo

--- registo.c
+++ registo.c
@@ -19,7 +19,7 @@ void guarda_saida_no_registo(Registo* registo, Data* data_saida) {
     registo->saida = data_saida;
 }
 
-void guarda_custo_no_registo(Registo* registo, int custo) {
+void guarda_custo_no_registo(Registo* registo, float custo) {
     registo->custo = custo;
 }
 
--- registo.h
+++ registo.h
@@ -17,7 +17,7 @@ typedef void (*Operacao_Registo)(Registo*);
 
 Registo* criar_registo(Parque* parque, Carro* carro, Data* entrada);
 void guarda_saida_no_registo(Registo* registo, Data* data_saida);
-void guarda_custo_no_registo(Registo* registo, int custo);
+void guarda_custo_no_registo(Registo* registo, float custo);
 void imprime_registo(Registo* registo);
 void libertar_registo(Registo* registo);
 
