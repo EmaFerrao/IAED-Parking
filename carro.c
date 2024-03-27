b6850cd7e7b317a0b40d25e759faa6c1730d167f
EmaFerrao <125159850+EmaFerrao@users.noreply.github.com>
2024-03-27 17:15:23 +0000

    .

--- carro.c
+++ carro.c
@@ -8,7 +8,7 @@
 
 Carro* cria_carro(char* matricula) {
     Carro* carro = (Carro*) malloc(sizeof(Carro));
-    carro->matricula = matricula;
+    carro->matricula = strdup(matricula);
     carro->dentro_de_parque = TRUE;
     carro->lista_registos = cria_lista_registos();
     return carro;
@@ -16,5 +16,6 @@ Carro* cria_carro(char* matricula) {
 
 void libertar_carro(Carro* carro) {
     libertar_lista_registos(carro->lista_registos, /*libertar_registos*/TRUE);
+    free(carro->matricula);
     free(carro);
 }
